9639f9a9b37193607de230c50403be73bc8b601c
swissKeys <becki_barth@yahoo.com>
2023-08-28 14:35:58 +0000

    collect data array works

--- examples/nulli.py
+++ examples/nulli.py
@@ -15,7 +15,67 @@ import time
 #TODO: func(array) return current
 #TODO: calc_volage(current) return voltage, power  
 
+def collect_array():
+    mocked_telemetry = [
+    {'index': 0, 'pos_x': 0.0, 'pos_y': 0.2224, 'pos_z': 0.0, 'mag_x': 0.1, 'mag_y': 0.2, 'mag_z': 0.5},
+    {'index': 1, 'pos_x': 0.0, 'pos_y': 0.2221, 'pos_z': 0.3, 'mag_x': 0.2, 'mag_y': 0.3, 'mag_z': 0.6},
+    {'index': 2, 'pos_x': 0.0, 'pos_y': 0.4, 'pos_z': 0.7, 'mag_x': 0.3, 'mag_y': 0.4, 'mag_z': 0.7},
+    {'index': 3, 'pos_x': 0.0, 'pos_y': 0.6, 'pos_z': 1.2, 'mag_x': 0.4, 'mag_y': 0.5, 'mag_z': 0.8},
+    {'index': 4, 'pos_x': 0.0, 'pos_y': 0.8, 'pos_z': 1.7, 'mag_x': 0.5, 'mag_y': 0.6, 'mag_z': 0.9},
+    {'index': 5, 'pos_x': 0.0, 'pos_y': 1.0, 'pos_z': 2.2, 'mag_x': 0.6, 'mag_y': 0.7, 'mag_z': 1.0}
+    ]
 
+    # Initialize index for new data points
+    current_index = len(mocked_telemetry)
+
+    # Simulate ongoing data flow
+    collecting_along_axis = True
+    number_of_datapoints = 0
+    increment_step_size = 0
+    data_point_results = []
+
+
+
+    while collecting_along_axis:
+        # Magnetometer data (in micro-Teslas):
+        #x,y,z = bno.read_magnetometer()
+        random_pos_z = random.uniform(1, 2)
+        random_variation = random.uniform(-0.02, 0.02)
+        latest_data_point = mocked_telemetry[current_index-1]
+        new_data_point = {
+            'index': current_index,
+            'pos_x': 0.0,
+            'pos_y': 4.567,
+            'pos_z': latest_data_point["pos_z"] + random_pos_z,
+            'mag_x': 0.6 + random_variation,
+            'mag_y': 0.7 + random_variation,
+            'mag_z': 1.0 + random_variation,
+        }
+
+        mocked_telemetry.sort(key=lambda data: data['pos_z'])
+        #print("Added new data point pos z:", new_data_point["pos_z"])
+        #print("Added new data point mag z:", new_data_point["mag_z"])
+
+        #print("latest data point:", latest_data_point)
+        #print("last pos_z", latest_data_point["pos_z"])
+        #print("new pos_z", new_data_point["pos_z"])
+        increment_step_size = increment_step_size + (new_data_point["pos_z"] - latest_data_point["pos_z"])
+        #print("increment step size", increment_step_size)
+        if increment_step_size >= 5: 
+            #TODO: add to array and increase number_of_datapoints
+            print("Sensor moved in 5 in pos z axis")
+
+            number_of_datapoints += 1
+            data_point_results.append({'pos_z': new_data_point["pos_z"], 'mag_z': new_data_point["mag_z"]})
+        #print("Added new data point:", new_data_point)
+        if len(data_point_results) == 5:
+            return data_point_results
+        time.sleep(1)  # Delay for 1 seconds
+        # Add the new data point to the telemetry list
+        mocked_telemetry.append(new_data_point)
+        
+        # Increment the index
+        current_index += 1
 
 def main():
     parser = argparse.ArgumentParser(description='Calculate voltage, power, and current')
@@ -26,57 +86,8 @@ def main():
     parser.add_argument('--measured_axis', type=str, required=False, default='z', help='Axis along which measurements are taken')
     args = parser.parse_args()
 
-mocked_telemetry = [
-    {'index': 0, 'pos_x': 0.0, 'pos_y': 0.2224, 'pos_z': 0.0, 'mag_x': 0.1, 'mag_y': 0.2, 'mag_z': 0.5},
-    {'index': 1, 'pos_x': 0.0, 'pos_y': 0.2221, 'pos_z': 0.3, 'mag_x': 0.2, 'mag_y': 0.3, 'mag_z': 0.6},
-    {'index': 2, 'pos_x': 0.0, 'pos_y': 0.4, 'pos_z': 0.7, 'mag_x': 0.3, 'mag_y': 0.4, 'mag_z': 0.7},
-    {'index': 3, 'pos_x': 0.0, 'pos_y': 0.6, 'pos_z': 1.2, 'mag_x': 0.4, 'mag_y': 0.5, 'mag_z': 0.8},
-    {'index': 4, 'pos_x': 0.0, 'pos_y': 0.8, 'pos_z': 1.7, 'mag_x': 0.5, 'mag_y': 0.6, 'mag_z': 0.9},
-    {'index': 5, 'pos_x': 0.0, 'pos_y': 1.0, 'pos_z': 2.2, 'mag_x': 0.6, 'mag_y': 0.7, 'mag_z': 1.0},
-    # ... Initial telemetry data ...
-]
-
-# Initialize index for new data points
-current_index = len(mocked_telemetry)
-
-# Simulate ongoing data flow
-collecting_along_axis = True
-number_of_datapoints = 0
-
-while collecting_along_axis:
-    # Magnetometer data (in micro-Teslas):
-    #x,y,z = bno.read_magnetometer()
-    random_pos_z = random.uniform(2.5, 10.0)
-    random_variation = random.uniform(-0.02, 0.02)
-
-    new_data_point = {
-        'index': current_index,
-        'pos_x': 0.0,
-        'pos_y': 4.567,
-        'pos_z': random_pos_z,
-        'mag_x': 0.6 + random_variation,
-        'mag_y': 0.7 + random_variation,
-        'mag_z': 1.0 + random_variation,
-    }
-
-    mocked_telemetry.sort(key=lambda data: data['pos_z'])
-    #print("Added new data point pos z:", new_data_point["pos_z"])
-    #print("Added new data point mag z:", new_data_point["mag_z"])
-
-    latest_data_point = mocked_telemetry[current_index-1]
-    print("latest data point:", latest_data_point)
-    if new_data_point["pos_z"] > latest_data_point["pos_z"] + 0.5: 
-        #TODO: add to array and increase number_of_datapoints
-        
-        print("Sensor moved in pos z axis", new_data_point)
-    #print("Added new data point:", new_data_point)
-    
-    time.sleep(1)  # Delay for 1 seconds
-    # Add the new data point to the telemetry list
-    mocked_telemetry.append(new_data_point)
-    
-    # Increment the index
-    current_index += 1
+    magneto_data_array = collect_array()
+    print(magneto_data_array)
 
     """ 
         telemetry_array, pos = collect_array(args.measured_axis)
