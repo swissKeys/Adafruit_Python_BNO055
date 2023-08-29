868cb3a5becaf71542665a401710216f5fbb4ebc
swissKeys <becki_barth@yahoo.com>
2023-08-29 15:47:53 +0000

    second fucntion

---
+++ examples/helpers.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.constants import *
+
+def gamma(p):
+    gamma0 = 0.5445
+    gamma1 = np.zeros(3)
+    gamma1[0] = (gamma0 + (p * gamma0) / 100)
+    gamma1[2] = (gamma0 - (p * gamma0) / 100)
+    gamma1[1] = gamma0
+    return gamma1
+
+def zi(le, z, p):
+    i = 0
+    z1= np.zeros((3, len(z)))
+    z2= np.zeros((3, len(z)))
+
+    while i<3:
+      j = 0
+      while j < len(z):
+        z1[i, j] = j + D(le,p)[i]/2
+        z2[i, j] = j - D(le,p)[i]/2
+        j+=1
+      i+=1
+    return z1, z2
+
+def D(le, p):
+    D = gamma(p) * le
+    return D
+
+def B_z_s(z, I, N, L):
+    return 4 * ((mu_0*I*N*L*L)/(np.pi)) * (1/(L*L + 4*z*z)) * (1/(2*L*L + 4*z*z)**0.5) * 1e4
--- examples/nulli.py
+++ examples/nulli.py
@@ -8,21 +8,23 @@ import argparse
 import time
 
 import random
-import time
+import numpy as np
+from scipy.constants import *
+from helpers import D, B_z_s, zi
 
 #TODO: reiceive mockeed telemetry from sensor (pos x,y,z) and magneticfield values(x,y,z). the sensor is facked moved along the z axis.
 #TODO: collect_array(axis) use ongoing telemertry from sensor for z-axis. Save the value for magnetfield (z) and pos (z) every 5cm the sensor moves into positove z-direction Until we have 10 values. return list.
 #TODO: func(array) return current
 #TODO: calc_volage(current) return voltage, power  
 
-def collect_array(axis, max_number):
+def collect_array(axis, number_of_datapoints, length_of_one_side):
     mocked_telemetry = [
-    {'index': 0, 'pos_x': 0.0, 'pos_y': 0.2224, 'pos_z': 0.0, 'mag_x': 0.1, 'mag_y': 0.2, 'mag_z': 0.5},
-    {'index': 1, 'pos_x': 0.0, 'pos_y': 0.2221, 'pos_z': 0.3, 'mag_x': 0.2, 'mag_y': 0.3, 'mag_z': 0.6},
-    {'index': 2, 'pos_x': 0.0, 'pos_y': 0.4, 'pos_z': 0.7, 'mag_x': 0.3, 'mag_y': 0.4, 'mag_z': 0.7},
-    {'index': 3, 'pos_x': 0.0, 'pos_y': 0.6, 'pos_z': 1.2, 'mag_x': 0.4, 'mag_y': 0.5, 'mag_z': 0.8},
-    {'index': 4, 'pos_x': 0.0, 'pos_y': 0.8, 'pos_z': 1.7, 'mag_x': 0.5, 'mag_y': 0.6, 'mag_z': 0.9},
-    {'index': 5, 'pos_x': 0.0, 'pos_y': 1.0, 'pos_z': 2.2, 'mag_x': 0.6, 'mag_y': 0.7, 'mag_z': 1.0}
+    {'index': 0, 'pos_x': 0.0, 'pos_y': 0.2224, 'pos_z': 0.0, 'mag_x': 0.6, 'mag_y': 0.7, 'mag_z': 0.5},
+    {'index': 1, 'pos_x': 0.0, 'pos_y': 0.2221, 'pos_z': 0.3, 'mag_x': 0.6, 'mag_y': 0.7, 'mag_z': 0.5},
+    {'index': 2, 'pos_x': 0.0, 'pos_y': 0.4, 'pos_z': 0.7, 'mag_x': 0.6, 'mag_y': 0.7, 'mag_z': 0.5},
+    {'index': 3, 'pos_x': 0.0, 'pos_y': 0.6, 'pos_z': 1.2, 'mag_x': 0.6, 'mag_y': 0.7, 'mag_z': 0.5},
+    {'index': 4, 'pos_x': 0.0, 'pos_y': 0.8, 'pos_z': 1.7, 'mag_x': 0.6, 'mag_y': 0.7, 'mag_z': 0.5},
+    {'index': 5, 'pos_x': 0.0, 'pos_y': 1.0, 'pos_z': 2.2, 'mag_x': 0.6, 'mag_y': 0.7, 'mag_z': 0.5}
     ]
 
     # Initialize index for new data points
@@ -30,26 +32,24 @@ def collect_array(axis, max_number):
 
     # Simulate ongoing data flow
     collecting_along_axis = True
-    number_of_datapoints = 0
+    max_increment_step = length_of_one_side/number_of_datapoints
     increment_step_size = 0
     data_point_results = []
-
-
 
     while collecting_along_axis:
         # Magnetometer data (in micro-Teslas):
         #x,y,z = bno.read_magnetometer()
         random_pos_z = random.uniform(1, 2)
-        random_variation = random.uniform(-0.02, 0.02)
+        #random_variation = random.uniform(-0.02, 0.02)
         latest_data_point = mocked_telemetry[current_index-1]
         new_data_point = {
             'index': current_index,
             'pos_x': 0.0,
             'pos_y': 4.567,
             'pos_z': latest_data_point["pos_"+ axis] + random_pos_z,
-            'mag_x': 0.6 + random_variation,
-            'mag_y': 0.7 + random_variation,
-            'mag_z': 1.0 + random_variation,
+            'mag_x': 0.6,
+            'mag_y': 0.7,
+            'mag_z': 0.5,
         }
 
         mocked_telemetry.sort(key=lambda data: data["pos_"+ axis])
@@ -61,14 +61,13 @@ def collect_array(axis, max_number):
         #print("new pos_z", new_data_point["pos_z"])
         increment_step_size = increment_step_size + (new_data_point["pos_"+ axis] - latest_data_point["pos_"+ axis])
         #print("increment step size", increment_step_size)
-        if increment_step_size >= max_number: 
-            #TODO: add to array and increase number_of_datapoints
-            print("Sensor moved in 5 in pos z axis")
 
-            number_of_datapoints += 1
+        if increment_step_size >= max_increment_step: 
+            print("Sensor moved in max_increment_step in pos z axis")
             data_point_results.append({"pos_"+ axis: new_data_point["pos_"+ axis], "mag_"+ axis: new_data_point["mag_"+ axis]})
-        #print("Added new data point:", new_data_point)
-        if len(data_point_results) == max_number:
+            increment_step_size = 0
+
+        if len(data_point_results) == number_of_datapoints:
             return data_point_results
         time.sleep(1)  # Delay for 1 seconds
         # Add the new data point to the telemetry list
@@ -77,19 +76,54 @@ def collect_array(axis, max_number):
         # Increment the index
         current_index += 1
 
+def calculate_current(axis, magneto_data_array, number_turns, length_of_one_side, distance_coils):
+
+    N = number_turns  #number of turns of wire
+    z = np.linspace(-0.5, 0.5, len(magneto_data_array)) #in meters
+    L = length_of_one_side
+    p = 0 
+    z1 = z + distance_coils/2
+    z2 = z - distance_coils/2
+    current_to_nulli = []
+    magnetic_field = []
+    print(z1)
+    print(z2)
+    for index, datapoint in enumerate(magneto_data_array): #is i for one axis bevause in teh code it is 1 
+         magnetic_field.append(datapoint["mag_"+ axis])
+         current_to_nulli.append(datapoint["mag_"+ axis]*(4 * ((mu_0*N*L*L)/(np.pi)) * (1/(L*L + 4*z1[index]*z1[index])) * (1/(2*L*L + 4*z1[index]*z1[index])**0.5) * 1e4  
+         + 4 * ((mu_0*N*L*L)/(np.pi)) * (1/(L*L + 4*z2[index]*z2[index])) * (1/(2*L*L + 4*z2[index]*z2[index])**0.5) * 1e4)**-1)
+    #print(z[j], B_z_s(z1[i,j], I, N, L) + B_z_s(z2[i,j], I, N, L), I_null[j])
+
+    #plt.plot(z, B_z_s(z1[i,], I, N, L) + B_z_s(z2[i,], I, N, L))
+    #plt.plot(I_null)
+
+    #nullifying
+    current_to_nulli_array = np.asarray(current_to_nulli)
+    magnetic_field_array = np.asarray(magnetic_field)
+    B_null = B_z_s(z1, current_to_nulli_array, N, L) + B_z_s(z2, current_to_nulli_array, N, L)
+    print(B_null)
+    print(magnetic_field_array)
+    null_array = magnetic_field_array - B_null
+    for value in null_array:
+        if int(value) != 0: 
+            print("failure")
+            return
+
+    return current_to_nulli_array
+
 def main():
     parser = argparse.ArgumentParser(description='Calculate voltage, power, and current')
     parser.add_argument('--resistance', type=float, required=False, default=0.1, help='Resistance of the wire')
-    parser.add_argument('--length_of_one_side', type=float, required=False, default=0.1, help='Length of one side of the coil')
-    parser.add_argument('--distance_coils', type=float, required=False, default=0.2, help='Distance between coils')
-    parser.add_argument('--number_turns', type=int, required=False, default=100, help='Number of turns in the coil')
+    parser.add_argument('--length_of_one_side', type=float, required=False, default=1, help='Length of one side of the coil')
+    parser.add_argument('--distance_coils', type=float, required=False, default=0.54, help='Distance between coils')
+    parser.add_argument('--number_turns', type=float, required=False, default=20.0, help='Number of turns in the coil')
     parser.add_argument('--measured_axis', type=str, required=False, default='z', help='Axis along which measurements are taken')
     parser.add_argument('--number_of_datapoints', type=str, required=False, default=5, help='Number of data points to be collected')
     args = parser.parse_args()
 
-    magneto_data_array = collect_array(args.measured_axis, args.number_of_datapoints)
-    print(magneto_data_array)
-
+    magneto_data_array = collect_array(args.measured_axis, args.number_of_datapoints, args.length_of_one_side)
+    current_array = calculate_current(args.measured_axis, magneto_data_array, args.number_turns, args.length_of_one_side, args.distance_coils)  
+    print(current_array)
     """ 
         telemetry_array, pos = collect_array(args.measured_axis)
         current = calc_current(telemetry_array, pos)
