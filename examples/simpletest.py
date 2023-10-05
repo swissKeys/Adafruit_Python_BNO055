10984152a21654223b85949dc870d4c38f30bca1
swisskeys <becki_barth@yahoo.com>
2023-10-05 12:14:32 +0200

    format fix

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -104,7 +104,7 @@ while True:
     #print('grav_x={0} grav_y={1} grav_z={2}'.format(x, y, z))
     # Sleep for a second until the next reading.
     mag_x,mag_y,mag_z = bno.read_magnetometer()
-    print('mag_x={0} mag_y={1} mag_z={2}'.format(x, y, z))
+    print('mag_x={0} mag_y={1} mag_z={2}'.format(mag_x,mag_y,mag_z))
     acc_x,acc_y,acc_z = bno.read_accelerometer()
     displacement_from_acceleration = calculate_displacement_from_acceleration([acc_x, acc_y, acc_z], 1.00)
 
